3ae20dbe4b4e989f2a002e4a29c30107303e672e
xixi fast <xia863277233@163.com>
2015-04-09 16:40:45 +0800

    add annotation and submit code2

--- xixiCode/code190.c
+++ xixiCode/code190.c
@@ -1,7 +1,6 @@
 uint32_t reverseBits(uint32_t n) {
-   int i,bin=0;
-     for (i = 0; i < 32; i++) 
-	 bin+=(n >> i & 1)<<(31-i);
-    return bin;
-    
+	int i, bin = 0;
+	for (i = 0; i < 32; i++)                 // n>>i & 1 mean take  a bit which is   last  one of the number 
+		bin += (n >> i & 1) << (31 - i);     // and then put in the first position of number,the last one should move 31 times,
+	return bin;								 // and the penult move 30times,and so on
 }
